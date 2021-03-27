d18352f8e6829464781633396526292339ff4d74
LOGAN LEE <dlfjgkssk1@naver.com>
2021-03-28 01:14:25 +0900

    add recoil, recoilize

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Wrapper } from './Wrapper.js';
+import { useRecoilValue } from 'recoil';
+
 import { Header } from './Header';
 import { Body } from './Body';
 import { Footer } from './Footer';
+import { isDarkTheme } from 'globalState';
 
+import { Wrapper } from './Wrapper';
 function App() {
 	document.title = 'Get infomation of how to be teacher in korea!';
+	const isDark = useRecoilValue(isDarkTheme);
+
 	return (
-		<Wrapper>
+		<Wrapper isDark={isDark}>
 			<div className='app-block1' onClick={() => window.location.reload()}>
 				Like <br />
 				To <br />
