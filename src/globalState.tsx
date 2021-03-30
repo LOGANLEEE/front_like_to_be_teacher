2b70465df8862b908f255c24330c42a8035df5bc
LOGAN LEE <dlfjgkssk1@naver.com>
2021-03-30 21:47:22 +0900

    -add user_info_ state,theme_info_state

--- src/globalState.tsx
+++ src/globalState.tsx
@@ -1,6 +1,22 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
-export const isDarkTheme = atom({
-	key: 'isDarkTheme',
-	default: false,
+export const theme_info_state = atom({
+	key: 'theme_info_state',
+	default: { is_dark: true, dark_back_ground: '#0e161f', white_back_ground: '#6b636a' },
+});
+
+export const user_info_state = atom({
+	key: 'user_info',
+	default: {
+		user_id: '',
+		user_name: '',
+		login: false,
+	},
+});
+
+export const test = atom({
+	key: 'test',
+	default: {
+		login: false,
+	},
 });
