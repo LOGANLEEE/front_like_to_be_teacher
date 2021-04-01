fbf867d917f8263a9d0348df4a791b9da19c7300
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-02 01:09:44 +0900

    imprv logged in prhase syncing with user_info value

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -3,10 +3,13 @@ import { Wrapper } from './Wrapper';
 import { Navigation } from 'Components/Navigation';
 import { Login } from 'Components/Login';
 import { ThemeToggle } from 'Components/ThemeToggle';
+import { useRecoilValue } from 'recoil';
+import { user_info_state } from 'globalState';
 
 interface Props {}
 
 export const Header: FunctionComponent<Props> = () => {
+	const user_info = useRecoilValue(user_info_state);
 	return (
 		<Wrapper>
 			<div className='header-block1'>
@@ -17,7 +20,9 @@ export const Header: FunctionComponent<Props> = () => {
 					<Login className='header-block3-item1' />
 					<ThemeToggle className='header-block3-item2' />
 				</div>
-				<div className='header-block4'>user info after logging in</div>
+				<div className='header-block4'>
+					{user_info.is_login ? `Wellcome back, ${user_info?.first_name}! ` : 'user info after logging in'}
+				</div>
 			</div>
 		</Wrapper>
 	);
