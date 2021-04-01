4ab679f0edca5ed1c9854f4826c7ba3b4b618e54
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-02 01:08:09 +0900

    add logout function impl destory modal if user logged in

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FunctionComponent } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
 import { user_info_state } from 'globalState';
 import airplanemode_active from 'asset/airplanemode_active-white-18dp.svg';
 import airplanemode_inactive from 'asset/airplanemode_inactive-white-18dp.svg';
@@ -13,11 +13,15 @@ interface Props {
 
 export const Login: FunctionComponent<Props> = (props) => {
 	const [should_open, set_should_open] = useState(true);
+	const { is_login } = useRecoilValue(user_info_state);
+
+	// ! logout function
+	const reset_user_info = useResetRecoilState(user_info_state);
 
 	return (
 		<Wrapper {...props}>
-			<StyledButton onClick={() => set_should_open(true)}>
-				{useRecoilValue(user_info_state).login ? (
+			<StyledButton onClick={() => (is_login ? reset_user_info() : set_should_open(true))}>
+				{is_login ? (
 					<>
 						LogOut
 						<img src={airplanemode_inactive} alt='login' />
@@ -29,7 +33,7 @@ export const Login: FunctionComponent<Props> = (props) => {
 					</>
 				)}
 			</StyledButton>
-			<LoginModal should_open={should_open} set_should_open={set_should_open} />
+			{is_login ? '' : <LoginModal should_open={should_open} set_should_open={set_should_open} />}
 		</Wrapper>
 	);
 };
