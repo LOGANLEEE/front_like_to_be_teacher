16bbbdeb0be09fa87c89fa60b9273bfdfdc0eeea
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-05 02:42:28 +0900

    mod menu type as optional enable dynamic comp

--- src/Components/Board/index.tsx
+++ src/Components/Board/index.tsx
@@ -3,13 +3,13 @@ import { Wrapper } from './Wrapper';
 
 interface Props {
 	className?: string;
-	menu: string;
+	menu?: string;
 }
 
 export const Board: FunctionComponent<Props> = ({ className, children, menu }) => {
 	return (
 		<Wrapper className={className}>
-			<div>{menu}</div>
+			{menu ? <div>{menu}</div> : null}
 			<div className='list'>{children}</div>
 		</Wrapper>
 	);
