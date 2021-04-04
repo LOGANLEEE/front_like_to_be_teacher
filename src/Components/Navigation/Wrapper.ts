349cfbafd055b8c03ca9fa58b0ce083dc362c3d2
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-05 02:44:49 +0900

    add set_menu to enable menu change
    render comps as iteration
    
    imprv css

--- src/Components/Navigation/Wrapper.ts
+++ src/Components/Navigation/Wrapper.ts
@@ -2,20 +2,22 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	display: flex;
-	flex-direction: row;
-	align-items: center;
+	flex-direction: column;
+	flex-wrap: wrap;
+	gap: 5px;
+
 	height: 100%;
+	width: 100%;
 
 	.item {
-		font-size: 1vw;
-		font-weight: bold;
-		/* overflow: auto; */
-		margin: 0% 1% 0% 1%;
-		cursor: pointer;
-		text-align: center;
-		height: 100%;
-		width: calc(100% / 5);
-		padding-left: 5px;
-		padding-right: 5px;
+		width: 100%;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		/* font-size: 1vw; */
+	}
+
+	.selected {
+		background-color: #5469d4;
 	}
 `;
--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -1,16 +1,27 @@
 import React, { FunctionComponent } from 'react';
 import { Wrapper } from './Wrapper';
 import { StyledButton } from 'Components/StyledButton';
+import { menu_state } from 'globalState';
+import { useRecoilState } from 'recoil';
+import { menus } from 'globalState';
 
 interface Props {}
 
 export const Navigation: FunctionComponent = ({}: Props) => {
+	const [{ selected }, set_menu] = useRecoilState(menu_state);
+
 	return (
 		<Wrapper>
-			<StyledButton className='item' text='Academy' />
-			<StyledButton className='item' text='News' />
-			<StyledButton className='item' text='Community' />
-			<StyledButton className='item' text='Q&A' />
+			{menus.map((text) => {
+				return (
+					<StyledButton
+						key={text}
+						className={`item ${selected === text ? 'selected' : ''}`}
+						text={text}
+						onClick={() => set_menu({ selected: text })}
+					/>
+				);
+			})}
 		</Wrapper>
 	);
 };
